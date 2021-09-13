0737ff8176851c0670e68d30f3c9fc2aa7a8c881
高津碧 <89884810+aoi-1100901@users.noreply.github.com>
2021-09-13 10:56:42 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,15 +18,15 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'あああ',
+          q1: '１９３７',
         },
         stage2: {
-          q1: 'いいい',
+          q1: 'ウイルス',
           // q2: 'えええ',
           // q3: 'おおお'
         },
         stage3: {
-          q1: 'ううう',
+          q1: 'SCIENCE',
           // q2: 'かかか',
           // q3: 'ききき',
         }
